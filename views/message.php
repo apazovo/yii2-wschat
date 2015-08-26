a5d7801dfaad1cca259ca238641f4e8b4a5e5807
apazovo <apazovo@gmail.com>
2015-08-26 16:45:00 +0300

    add class fff

--- views/message.php
+++ views/message.php
@@ -1,5 +1,5 @@
 <script id="msg-tpl" type="text/template">
-    <dl class="dl-horizontal">
+    <dl class="dl-horizontal fff">
         <dt>
         <div class="chat-item">
             <span><%=username%></span><img src="<%=avatar_16%>" width="16" height="16" class="img-circle">
